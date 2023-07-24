09f09e5658e06eca39d617e56cde8aee9f32f799
avinash <yaddlapalliavinash@gmail.com>
2023-07-24 11:33:01 +0530

    updated

--- AuthInMern/server/db.js
+++ AuthInMern/server/db.js
@@ -1,22 +1,29 @@
 const mongoose = require("mongoose");
 
 module.exports = () => {
+  // Connection parameters for MongoDB
   const connectionParams = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   };
 
+  // Replace the following variables with your MongoDB Atlas credentials
   const username = "Avinash2489";
-  const password = "Avinash@2489"; 
+  const password = "Avinash@2489";
   const dbName = "SOCIALBOOK1";
 
+  // Encode the password to handle special characters
   const encodedPassword = encodeURIComponent(password);
+
+  // MongoDB connection URI, including credentials and database name
   const uri = `mongodb+srv://${username}:${encodedPassword}@cluster1.hudmw3h.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
   try {
+    // Attempt to connect to the MongoDB Atlas database using Mongoose
     mongoose.connect(uri, connectionParams);
     console.log("Connected to database successfully");
   } catch (error) {
+    // If an error occurs during the connection attempt, log the error message
     console.log(error);
     console.log("Could not connect to the database!");
   }
--- AuthInMern/server/index.js
+++ AuthInMern/server/index.js
@@ -1,26 +1,34 @@
+// Load environment variables from .env file
 require("dotenv").config();
+
+// Import required modules
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
+// Import the database connection function from the db.js file
 const connection = require("./db");
 
-// Database connection
+// Establish a connection to the database using the connection function
 connection();
 
-// Middlewares
+// Middleware setup
+// Parse incoming JSON data in requests
 app.use(express.json());
+// Enable CORS (Cross-Origin Resource Sharing) to allow requests from different origins
 app.use(cors());
 
-// Routes
+// Import and set up the routes for the user and authentication endpoints
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
-
-
+// These routes will be accessible at /api/users and /api/auth respectively
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 
-
+// Define the port on which the server will listen, using the PORT environment variable if available, or defaulting to port 8080
 const port = process.env.PORT || 8080;
+
+// Start the server and listen on the specified port
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
